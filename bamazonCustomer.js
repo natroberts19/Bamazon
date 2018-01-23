d4b4c16f03d124c9bd10b4c9c5e95dd11afa0550
Natalie Roberts <nr_roberts@yahoo.com>
2018-01-22 19:33:08 -0500

    created product diplay, select query

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,13 +10,46 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "",
-  database: "greatBay_DB"
+  password: "HeBumps@15",
+  database: "bamazon_db"
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
+  console.log("connected as id " + connection.threadId);
   // run the start function after the connection is made to prompt the user
-  start();
+  // startTrans();
 });
+
+// Create a startTrans function to start the transaction.
+function startTrans() {
+  // Display the list of products that are available.
+  connection.query("SELECT * FROM products", function (err, results) {
+      // if (err) throw err;
+      for (var i = 0; i < results.length; i++) {
+        console.log("Item Id: " + results[i].item_id + " | Item Name: " + results[i].product_name + " | Item Price: " + results[i].price_cust_cost);
+      }
+      console.log("----------------------------------------------------------------------");
+    });
+
+    // Inquirer menu:
+    // Prompt 1. Enter the item_id for the product you want to buy. [input]
+    // Add conditional for valid entry.
+    // Run a query to select the item desired. (SELECT).
+
+    // Prompt 2. How many would you like to buy? [input]
+    // Run a query to determine if inventory is available. -- checkInv() function? or, just run a query here.
+    // If inventory not available, display message "Sorry there is not enough in inventory to fill your order."
+    // If inventory is available, run fillOrder().
+  }
+
+  // Create a function to check remaining inventory in the products table. ?
+  // function checkInv() {
+
+
+
+  // Create a function to fill the customer's order and update the inventory in the products table.
+  // function fillOrder() {
+  // Update the inventory ("UPDATE auctions SET ? WHERE ?")
+  // Fill the order, "Order Successfully submitted. You have purchased +quantity+ of +item+. Your order total is: +order_total+."
--- bamazon_seed.sql
+++ bamazon_seed.sql
@@ -5,9 +5,23 @@ USE bamazon_db;
 
 CREATE TABLE products(
   id INTEGER(11) unsigned NOT NULL auto_increment PRIMARY KEY,
-  item_id INT UNIQUE NOT NULL,
+  item_id VARCHAR(55) UNIQUE NOT NULL,
   product_name VARCHAR(100) NOT NULL,
-  department_name VARCHAR(45),
-  price_cust_cost INT default 0,
-  stock_quantity INT
+  department_name VARCHAR(55),
+  price_cust_cost INTEGER DECIMAL(0,2) default 0,
+  stock_quantity INTEGER
 );
+
+INSERT INTO products
+	(item_id, product_name, department_name, price_cust_cost, stock_quantity)
+    VALUES 
+    ('12item1','Mizuno Wave Runner 10', 'Clothing, Shoes', '89.00', '20'),
+    ('56item2','Orthopedic Pet Bed', 'Pet Supplies', '25.00', '16'),
+    ('56item3','Dog Car Seat Cover', 'Pet Supplies', '28.00', '8'),
+    ('34item4','CamelBak Water Bottle', 'Sports, Outdoors', '24.00', '3'),
+    ('34item5','HS Backpack', 'Sports, Outdoors', '124.00', '5'),
+    ('12item6','Brooks Adrenaline GTS', 'Clothing, Shoes', '68.00', '13'),
+    ('78item7','Nikon D5600 Digital SLR', 'Electronics', '564.00', '9'),
+    ('12item8','Coastal Blue Colorblock Swimwear', 'Clothing, Shoes', '39.00', '11'),
+    ('90item9','GoYoga Yoga Mat', 'Sports, Fitness', '16.00', '33'),
+    ('90item10','FitBit Smart Fitness Watch', 'Sports, Fitness', '189.00', '1');
